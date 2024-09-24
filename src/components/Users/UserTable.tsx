34be8e5290b9f72ce9caf86382b9e2b867bbf2e7
anurag Pattanaik <anurag.pattanaik@oasystspl.com>
2024-09-24 18:57:46 +0530

    commit

--- src/components/Users/UserTable.tsx
+++ src/components/Users/UserTable.tsx
@@ -1,27 +1,45 @@
 import React from "react";
 import { User } from "../../interfaces/user";
-interface UserTableProps{
-    users:User[]
+import { Table } from "react-bootstrap";
+interface UserTableProps {
+  users: User[];
 }
-const UserTable:React.FC<UserTableProps> = ({users}) =>{
-    return (
-
-        
-        <table className="table table-bordered">
-        {
-            users.map((user,key)=>(
+const UserTable: React.FC<UserTableProps> = ({ users }) => {
+  return (
+    <div className="container2">
+      <div className="col-md-12 mt-2">
+        <div className="card">
+          <div className="card-body">
+            <h5>User List</h5>
+            <Table bordered hover striped variant="info">
+              <thead>
                 <tr>
+                  <th>Sl. No.</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Phone</th>
+                  <th>Designation</th>
+                  <th>Address</th>
+                </tr>
+              </thead>
+              <tbody>
+                {users.map((user, key) => (
+                  <tr key={user.id}>
                     <td>{++key}</td>
                     <td>{user.name}</td>
                     <td>{user.email}</td>
                     <td>{user.phone}</td>
                     <td>{user.designation}</td>
                     <td>{user.address}</td>
-                </tr>
-            ))
-        }
-    </table>
-    )
-}
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default UserTable;
